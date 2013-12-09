978c1c5fb70f6eda7707a928685df3cc9e7f4d18
Mohammed Khalilia <mohammed.khalilia@gmail.com>
2013-12-09 10:51:04 -0600

    update

--- som_demo.m
+++ som_demo.m
@@ -1,21 +1,23 @@
-%load the dataset, see som_dataset.m for details
-dataset = som_dataset({'ws3g'});
+clear;
 
-input.data              = dataset.objectData;
-input.alg               = 'BATCH';
+%load the dataset, see som_dataset.m for details
+dataset = som_dataset({'cvr'});
+
+input.data              = dataset.relationalData;
+input.alg               = 'RELATIONALFUZZY';
 input.maxIter           = 10;
 input.dim               = dataset.mapsize;
 input.radius            = [2 0.5];
-%input.fuzzifier         = [1.01 2];
+input.fuzzifier         = [1.1 1.5];
 input.weightsInitFun    = 1;
         
 map = som(input);
 
 %compute the topographic and quantization errors
-%[qe te] = quality(map);
+[qe te] = quality(map);
 
 %compute the fuzzy topographic error (only for the fuzzy versions of SOM)
-%[tef ~] = fuzzy_topographic_error(map);
+[tef ~] = fuzzy_topographic_error(map);
         
 %summarize the map
 figure;summarization(map, dataset);
